d7340dbefa9a7a36d693f5e93797c3748915b023
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2025-03-24 22:00:06 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='"App"'>
+        <h1>Hello Hien's App</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App;
